5de9b32db1c779fa8c6672abc7db1459843dc7d4
Dmitriy Akishin <dakishin@gmail.com>
2021-06-02 02:22:00 +0300

    comit

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -1,3 +1,21 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        dependencies {
+            classpath("com.android.tools.build:gradle:4.2.1")
+            classpath(kotlin("gradle-plugin", version = "1.5.0"))
+
+            // NOTE: Do not place your application dependencies here; they belong
+            // in the individual module build.gradle.kts files
+        }
+
+    }
+}
+
+
 plugins {
     id("com.android.library")
     kotlin("android")
